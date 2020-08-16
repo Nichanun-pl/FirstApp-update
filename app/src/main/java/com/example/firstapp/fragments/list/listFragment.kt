c9b973103088aac80e758c1a4a0710e3d02da37c
Nichanun <nichanun.pl@rmuti.ac.th>
2020-08-16 23:02:40 +0700

    update frament

--- app/src/main/java/com/example/firstapp/fragments/list/listFragment.kt
+++ app/src/main/java/com/example/firstapp/fragments/list/listFragment.kt
@@ -1,10 +1,11 @@
-package com.example.firstapp
+package com.example.firstapp.fragments.list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.firstapp.R
 
 class listFragment : Fragment() {
     override fun onCreateView(
