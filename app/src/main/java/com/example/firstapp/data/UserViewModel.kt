194ccd736b3ad3ba2101af4cfa312501c030b6ce
Nichanun <nichanun.pl@rmuti.ac.th>
2020-08-16 22:40:18 +0700

    Update UserViewModel.kt

--- app/src/main/java/com/example/firstapp/data/UserViewModel.kt
+++ app/src/main/java/com/example/firstapp/data/UserViewModel.kt
@@ -1,4 +1,26 @@
-package com.example.firstapp.data
+package com.example.firstapp.Database
 
-class UserViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class UserViewModel(application: Application): AndroidViewModel(application) {
+
+    private val readAllData: LiveData<List<User>>
+    private val repository: UserRepository
+
+    init {
+        val userDao = UserDatabase.getDatabase(application).userDao()
+        repository = UserRepository(userDao)
+        readAllData = repository.readAllDate
+    }
+
+    fun addUser(user: User){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.addUser(user)
+        }
+    }
 }
